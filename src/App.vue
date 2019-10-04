2cf1f73ebe469d3ccc6ad7efc4e87d937486473c
Sinisa Milicic <sinisamilicic@Sinisas-MacBook-Air.local>
2019-10-04 14:36:42 +0200

    Implemented Vue Router.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,15 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo @add-todo="addTodo" />
-    <Todos :todos="todos" @del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
-
 export default {
   name: "app",
-  components: { Todos, Header, AddTodo },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(res => (this.todos = this.todos.filter(todo => todo.id != id)))
-        .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-
-      axios
-        .post("https://jsonplaceholder.typicode.com/todos", {
-          title,
-          completed
-        })
-        .then(res => (this.todos = [...this.todos, res.data]))
-        .catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then(res => (this.todos = res.data))
-      .catch(err => console.log(err));
-  }
+  components: { Header }
 };
 </script>
 
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <header class="header">
     <h1>TodoList</h1>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-    </div>-->
+    </div>
   </header>
 </template>
 
