b4f9fe41c9b11fd43a8fa05bd9bce051b4f8b9eb
Sinisa Milicic <sinisamilicic@Sinisas-MacBook-Air.local>
2019-10-04 14:22:25 +0200

    Implemented axios and ajax calls to JSONPlaceholder

---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem :todo="todo" @del-todo="$emit('del-todo', todo.id )" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: { TodoItem },
+  props: ["todos"]
+};
+</script>
